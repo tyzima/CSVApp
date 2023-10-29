ba1611e111a13f2e3214dc636cfe9a6744e8f6d8
tyzima <ty@zimagear.com>
2023-10-28 21:39:10 -0400

    Test

--- app.js
+++ app.js
@@ -336,24 +336,49 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('csv-form').dispatchEvent(new Event('submit', { 'bubbles': true }));
   }
 
-  async function sendToSalesforce(aggregatedData) {
-    console.log("Sending to Salesforce...", aggregatedData);
-    const zapierWebhookUrl = 'https://hooks.zapier.com/hooks/catch/53953/38lmops/'; // Replace with your actual Zapier webhook URL
+ async function getProductJSON() {
     try {
-        const productJSON = await getProductJSON(); // Assume this function is defined elsewhere to fetch ProductJSON.json
+        const response = await fetch('ProductJSON.json');
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching product JSON:', error);
+        throw error; // Re-throw the error to be handled by the calling function
+    }
+}
+
+async function sendToSalesforce(aggregatedData) {
+    // Define the URL of your Zapier webhook
+    const zapierWebhookUrl = 'https://hooks.zapier.com/hooks/catch/53953/38lmops/';
+    
+    try {
+        // Fetch the product data
+        const productJSON = await getProductJSON();
+        
+        // Loop through each item in the aggregated data
         for (const item of Object.values(aggregatedData)) {
+            // Extract the 'Style-Size' and 'Aggregated Quantity' values from the current item
             const productCode = item['Style-Size'];
             const quantityAggregated = item['Aggregated Quantity'];
+            
+            // Find the corresponding product in the product data
             const product = productJSON.find(p => p['Product Code'] === productCode);
+            
+            // Get the '18CharID' value from the product, or set it to an empty string if the product wasn't found
             const charID = product ? product['18CharID'] : '';
-
+            
+            // Create the payload to send to Zapier
             const payload = {
                 'Style-Size': productCode,
                 'Quantity Aggregated': quantityAggregated,
                 '18CharID': charID
             };
-
+            
             try {
+                // Send the payload to the Zapier webhook
                 const response = await fetch(zapierWebhookUrl, {
                     method: 'POST',
                     headers: {
@@ -361,13 +386,17 @@ document.addEventListener('DOMContentLoaded', () => {
                     },
                     body: JSON.stringify(payload),
                 });
+                
+                // Parse the response from Zapier
                 const responseData = await response.json();
                 console.log('Success:', responseData);
             } catch (error) {
+                // Log any errors that occurred while sending data to Zapier
                 console.error('Error sending data to Zapier:', error);
             }
         }
     } catch (error) {
+        // Log any errors that occurred while processing data for Zapier
         console.error('Error processing data for Zapier:', error);
     }
 }
