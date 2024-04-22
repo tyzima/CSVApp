e8f6b6f170255398b5cdf247fd8d54126f527829
tyzima <41972512+tyzima@users.noreply.github.com>
2024-04-21 20:55:55 -0400

    Send

--- netlify/functions/sendToAirtable.js
+++ netlify/functions/sendToAirtable.js
@@ -1,11 +1,16 @@
 const axios = require('axios');
+const { chunk } = require('lodash'); // Ensure lodash is installed
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 exports.handler = async (event) => {
   // Setting CORS headers to allow only requests from your specific domain
   const headers = {
-    'Access-Control-Allow-Origin': 'https://omgcleanup.netlify.app', // Allow only this domain
+    'Access-Control-Allow-Origin': 'https://omgcleanup.netlify.app',
     'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS' // Adjust methods according to your needs
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
   };
 
   // Handling OPTIONS method for preflight requests
@@ -27,6 +32,7 @@ exports.handler = async (event) => {
 
   const payload = JSON.parse(event.body);
   const { records } = payload;
+  const batches = chunk(records, 10); // Split records into batches of 10
 
   const config = {
     headers: {
@@ -36,11 +42,16 @@ exports.handler = async (event) => {
   };
 
   try {
-    const response = await axios.post('https://api.airtable.com/v0/appJaZT3HpcWc1IFL/upload', { records }, config);
+    const responses = [];
+    for (const batch of batches) {
+      const response = await axios.post('https://api.airtable.com/v0/appJaZT3HpcWc1IFL/upload', { records: batch }, config);
+      responses.push(response.data);
+      await delay(200); // Delay to prevent hitting the rate limit
+    }
     return {
       statusCode: 200,
       headers,
-      body: JSON.stringify(response.data)
+      body: JSON.stringify(responses)
     };
   } catch (error) {
     return {
