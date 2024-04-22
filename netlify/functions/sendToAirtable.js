f12d5cb56477907530b4d4ab4742c0d23afce16b
tyzima <41972512+tyzima@users.noreply.github.com>
2024-04-21 20:34:32 -0400

    Added Post options

--- netlify/functions/sendToAirtable.js
+++ netlify/functions/sendToAirtable.js
@@ -1,8 +1,28 @@
 const axios = require('axios');
 
 exports.handler = async (event) => {
+  // Setting CORS headers to allow only requests from your specific domain
+  const headers = {
+    'Access-Control-Allow-Origin': 'https://omgcleanup.netlify.app', // Allow only this domain
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS' // Adjust methods according to your needs
+  };
+
+  // Handling OPTIONS method for preflight requests
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers,
+      body: ''
+    };
+  }
+
   if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+    return { 
+      statusCode: 405, 
+      headers,
+      body: 'Method Not Allowed' 
+    };
   }
 
   const payload = JSON.parse(event.body);
@@ -19,12 +39,14 @@ exports.handler = async (event) => {
     const response = await axios.post('https://api.airtable.com/v0/appJaZT3HpcWc1IFL/upload', { records }, config);
     return {
       statusCode: 200,
+      headers,
       body: JSON.stringify(response.data)
     };
   } catch (error) {
     return {
-      statusCode: error.response.status,
-      body: JSON.stringify(error.response.data)
+      statusCode: error.response ? error.response.status : 500,
+      headers,
+      body: JSON.stringify(error.response ? error.response.data : { message: 'Internal Server Error' })
     };
   }
 };
